1fd2252f35dc4ba76ef2ce319787ef62bd71d437
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2021-04-28 23:45:26 +0500

    Working on It

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let js = document.getElementById("JS");
-let reactjs = document.getElementById("reactjs");
+const js = document.getElementById("JS");
+const reactDiv = document.getElementById("reactjs");
+const heading = document.getElementById("head");
+``
 
 const App = () =>
 {
@@ -8,5 +10,20 @@ const App = () =>
     JAVASCRIPT FORMAT / DOM
     <input /> 
     <p>${new Date()}</p>
-    </div>`  
+    </div>`;
+
+    var ReactJs = React.createElement(
+            "div",
+            {className: "split"},
+            "React JS / Virtual DOM",
+            React.createElement("input"),
+            React.createElement(
+                "p",
+                null,
+                new Date().toString()
+            )
+    );
+
+    ReactDOM.render(ReactJs,reactDiv);
 };
+setInterval(App, 1000);
